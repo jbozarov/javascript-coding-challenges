fd0097a180f3f121365922ba08fda45922b5359a
Jahon Bozarov <53405754+jbozarov@users.noreply.github.com>
2021-05-08 12:06:49 -0400

    Create SumOfStrings.js

---
+++ Numbers/SumOfStrings.js
@@ -0,0 +1,22 @@
+Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 -->Output)
+
+"4",  "5" --> "9"
+"34", "5" --> "39"
+
+
+
+
+
+function sumStr(a,b) {
+  if (a == "") {
+    a = 0; 
+  }
+  if (b == "") {
+    b = 0; 
+  }
+  const result = (parseInt(a) + parseInt(b)).toString(); 
+  console.log(result, a, b); 
+  return result; 
+}
