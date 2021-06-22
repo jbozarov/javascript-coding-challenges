e044f8e7e45d412ecdc36d1543f31286d01e9826
Jahon Bozarov <53405754+jbozarov@users.noreply.github.com>
2021-06-21 21:19:51 -0400

    Update and rename Sum Arrays to SumArrays.js

--- arrays/SumArrays.js
+++ arrays/SumArrays.js
@@ -1,12 +1,11 @@
-Write a method sum (sum_array in python, SumArray in C#) that takes an array of numbers and returns the sum of the numbers. 
+/* Write a method sum (sum_array in python, SumArray in C#) that takes an array of numbers and returns the sum of the numbers. 
 These may be integers or decimals for Ruby and any instance of Num for Haskell. The numbers can also be negative. If the array 
 does not contain any numbers then you should return 0.
 
 Examples
 numbers = [1, 5.2, 4, 0, -1]
 puts sum(numbers)
-9.2
-
+9.2 */
 
 function sum (numbers) {
     "use strict";
