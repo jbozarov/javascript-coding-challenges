d74de2dc3276ef94a283e737c9913a47154c06d5
Jahon Bozarov <53405754+jbozarov@users.noreply.github.com>
2021-06-21 21:34:10 -0400

    Update and rename Sum of differences in array to SumOfDifferencesInArray.js

--- arrays/SumOfDifferencesInArray.js
+++ arrays/SumOfDifferencesInArray.js
@@ -1,8 +1,8 @@
-Your task is to sum the differences between consecutive pairs in the array in descending order.
+/* Your task is to sum the differences between consecutive pairs in the array in descending order.
 For example: sumOfDifferences([1, 2, 10] Returns 9
 Descending order: [10, 2, 1]
 Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
-If the array is empty or the array has only one element the result should be 0.
+If the array is empty or the array has only one element the result should be 0. */ 
 
 
 
