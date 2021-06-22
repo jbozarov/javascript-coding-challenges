ab891c0ef319625b5d6cf2d8c9fc569e31021747
jbozarov <tech@jbozarov.com>
2021-06-21 21:38:22 -0400

    code optimized

--- date/ReturnADay.js
+++ date/ReturnADay.js
@@ -1,4 +1,4 @@
-Complete the function which returns the weekday according to the input number:
+/* Complete the function which returns the weekday according to the input number:
 
 1 returns "Sunday"
 2 returns "Monday"
@@ -7,7 +7,7 @@ Complete the function which returns the weekday according to the input number:
 5 returns "Thursday"
 6 returns "Friday"
 7 returns "Saturday"
-Otherwise returns "Wrong, please enter a number between 1 and 7"
+Otherwise returns "Wrong, please enter a number between 1 and 7" */ 
 
 
 function whatday(num) { 
