93c33ba2c64385ccc91cccfcc4b076bc75e0d5b7
Jahon Bozarov <53405754+jbozarov@users.noreply.github.com>
2021-06-22 11:55:09 -0400

    Update and rename binaryFake.js to BinaryFake.js

--- numeric systems/BinaryFake.js
+++ numeric systems/BinaryFake.js
@@ -1,4 +1,4 @@
-/ * Given a string of digits, you should replace any digit below 
+/* Given a string of digits, you should replace any digit below 
 5 with '0' and any digit 5 and above with '1'. Return the resulting string. */ 
 
 function fakeBin(x){
