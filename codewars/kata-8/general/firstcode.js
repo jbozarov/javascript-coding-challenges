62523fe92de83a72ba86929541e68a1dcd8bb28c
jbozarov <tech@jbozarov.com>
2021-06-24 22:47:48 -0400

    code file system optimized

---
+++ codewars/kata-8/general/firstcode.js
@@ -0,0 +1,16 @@
+function threeInOne (arr){
+	var newArr = []; 
+  for (var i=0; i<=arr.length; i+3){		
+		newArr.push(arr.slice(i, i+3)); 		
+	}
+	 return newArr;  
+}
+
+threeInOne([1,2,3,4,5,6]);
+
+ console.log('kfkd'); 
+
+ this.x = x; 
+ this.y = y; 
+ this.z = z ;\
+ 
