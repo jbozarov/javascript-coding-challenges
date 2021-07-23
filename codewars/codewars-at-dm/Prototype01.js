0b99b9f8a5086cfa0a3badf0b212cb62ef7bb36d
Jahon Bozarov <53405754+jbozarov@users.noreply.github.com>
2021-07-23 17:07:39 -0400

    Update and rename Prototype 01 to Prototype01.js

--- codewars/codewars-at-dm/Prototype01.js
+++ codewars/codewars-at-dm/Prototype01.js
@@ -1,7 +1,8 @@
-Here we have a constructor function named Restaurant that has a method inside the function.
+/* Here we have a constructor function named Restaurant that has a method inside the function.
 
 Remove the existing method addStars and use it to create a prototype method for this constructor 
 function. For testing purposes, make sure to use the same name.
+*/ 
 
 
 function Restaurant (name, type, stars) {
